413499e63eebc4991fbe5b6f9d627a9c526b2aea
Recep GÜNEY <recepguney@msn.com>
2022-08-12 23:38:08 +0300

    TxnTransactionService

--- src/app/services/transport/txn/transportTxn-service.module.ts
+++ src/app/services/transport/txn/transportTxn-service.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { TxnTransactionService } from './txnTransaction-service';
 
 
 @NgModule({
 	providers: [
+		TxnTransactionService,
 	],
 })
 export class TransportTxnServiceModule { }
