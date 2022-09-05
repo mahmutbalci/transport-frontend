f5713b0d11d74e846a10244bf6840313684476ec
mahmut.balci <mahmut.balci@birlesikodeme.com>
2022-09-05 12:12:18 +0300

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
 	production: true,
 	envName: 'PROD',
 	showEnvName: true,
-    baseUrl: 'https://propay.protel.com.tr:1908/api',
-    commonApiUrl: 'https://propay.protel.com.tr:1908/api',
-    frameworkApiUrl: 'https://propay.protel.com.tr:1908/api',
-    transportApiUrl: 'https://propay.protel.com.tr:1908/api',
+    baseUrl: 'https://propay.protel.com.tr/Transportation/api',
+    commonApiUrl: 'https://propay.protel.com.tr/Transportation/api',
+    frameworkApiUrl: 'https://propay.protel.com.tr/Transportation/api',
+    transportApiUrl: 'https://propay.protel.com.tr/Transportation/api',
     crystalQuartzUrl: 'https://propay.protel.com.tr:8088/quartz/',
 };
