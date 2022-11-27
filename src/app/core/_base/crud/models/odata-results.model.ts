ead973f722fa472efd1d0a2e5ec6661e439129eb
Recep GÜNEY <recepguney@msn.com>
2022-11-27 21:58:35 +0300

    this.items = httpResponse.body.data

--- src/app/core/_base/crud/models/odata-results.model.ts
+++ src/app/core/_base/crud/models/odata-results.model.ts
@@ -5,7 +5,7 @@ export class ODataResultsModel {
 	error: any;
 	
 	constructor(httpResponse:any,  _error?: any) {
-		this.items = httpResponse.body.result;
+		this.items = httpResponse.body.data;
 		this.totalCount = httpResponse.headers.get('h-item-count');
 		this.error = _error;
 	}
